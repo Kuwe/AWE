7d92e5ed6f0f212ae9a5a963bf62723be22e37b8
Kai-Uwe Lubosch <khanke@tzi.de>
2013-02-06 14:43:05 +0100

    User can see after enter a number the Fibonacci sequence

---
+++ LearningRuby/LearningRuby/fibonacci.rb
@@ -0,0 +1,34 @@
+puts "Geben Sie eine Zahl ein. Es wird eine Fibonacci folge erstellt mit der Anzahl der eingegeben Zahl. "
+
+
+while true
+	begin
+	number = Integer(gets.chomp)
+	break
+
+	rescue ArgumentError
+		puts "Bitte geben Sie eine Zahl ein!"
+	end
+end
+
+increaseFibo = true
+fibo1 = 1
+fibo2 = 2
+counter = 0;
+puts fibo1
+puts fibo2
+
+while counter <= number
+	if increaseFibo == false
+		fibo2 = fibo1 + fibo2
+		puts fibo2
+		increaseFibo = true
+	else if increaseFibo == true
+		fibo1 = fibo1 + fibo2
+		puts fibo1
+		increaseFibo = false
+		end
+	end
+	
+	counter = counter + 1
+end
