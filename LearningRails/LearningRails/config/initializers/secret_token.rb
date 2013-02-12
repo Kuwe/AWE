d80f03958b8f7b5c5fc83723aea795913c4376e8
Kai-Uwe Lubosch <khanke@informatik.uni-bremen.de>
2013-02-12 16:11:22 +0100

    First rails app

---
+++ LearningRails/LearningRails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningRails::Application.config.secret_token = '039156c0c6d0ff86cd7aaa9a90e7d674d1a9be5ea70af7dc68d907c5afc51467cee9d21b7ebd14bd8bfb3fa310c3c7b5673a3862b02c0b01af4e5fd9ab00f3cf'
